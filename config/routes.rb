26d2a3a0df7c6b387d44a3d0a788058600ac6c62
Rodolfo Campos <rodolfo123@gmail.com>
2022-11-14 14:21:30 -0300

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'questions#ask'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
